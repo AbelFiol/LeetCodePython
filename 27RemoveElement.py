3ad14fe7dbd816ebd703d8c3c1bec6e8e6644379
AbelFiol <abelfiol2712@gmail.com>
2024-10-09 10:51:57 -0500

    Remove Element

--- 27RemoveElement.py
+++ 27RemoveElement.py
@@ -2,9 +2,13 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0  # Initialize a pointer to keep track of the length of the result.
+        k = 0  # Pointer to keep track of the index for non-val elements.
+
+        # Iterate through each element in the nums list.
         for i in range(len(nums)):
+            # If the current element is not equal to the value to remove.
             if nums[i] != val:
-                nums[k] = nums[i]  # Move the element to the front of the list.
-                k += 1
-        return k
+                nums[k] = nums[i]  # Place the element at the next position of k.
+                k += 1  # Move the pointer k to the right.
+
+        return k  # Return the new length of the modified list.
