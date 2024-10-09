463209a6acf246f7f326cf74479d65a9536935b5
AbelFiol <abelfiol2712@gmail.com>
2024-10-09 10:30:39 -0500

    Merge Sorted Array

--- 88MergeSortedArray.py
+++ 88MergeSortedArray.py
@@ -2,19 +2,13 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        # Merge elements from both arrays in descending order.
-        i, j, k = m - 1, n - 1, m + n - 1
-        while i >= 0 and j >= 0:
-            if nums1[i] >= nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
+        i, j, k = m - 1, n - 1, m + n - 1  # Set pointers for nums1, nums2, and the last position in nums1.
 
-        # If there are remaining elements in nums2, copy them.
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
+        while j >= 0:  # Continue until all elements in nums2 are processed.
+            if i >= 0 and nums1[i] > nums2[j]:  # If nums1 has elements left and nums1[i] is larger, copy it.
+                nums1[k] = nums1[i]
+                i -= 1  # Move the pointer for nums1 to the left.
+            else:
+                nums1[k] = nums2[j]  # Otherwise, copy nums2[j] into nums1.
+                j -= 1  # Move the pointer for nums2 to the left.
+            k -= 1  # Move the merge pointer to the left.
